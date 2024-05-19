4b3200b82d79fe2beff54bdf0c27d21afd5c77a3
Dzkzzz <dzdzaakk@gmail.com>
2024-05-19 14:54:07 +0700

    create helper functions to deal with JSON

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -17,7 +16,6 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "Hit the broker",
 	}
 
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payload)
+
 }
